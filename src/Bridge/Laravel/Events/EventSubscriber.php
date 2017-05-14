66eb22f29289c2bd67652dc5c3b9d65a72cbb8e8
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2017-05-14 11:36:36 +0100

    Feature event listener (#1)
    * Added event listener
    
    * Updated event listener to cache gpio after request handled
    
    * Updated readme

---
+++ src/Bridge/Laravel/Events/EventSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ChickenTikkaMasala\GPIO\Bridge\Laravel\Events;
+
+use ChickenTikkaMasala\GPIO\Bridge\Laravel\GPIOServiceProvider;
+use ChickenTikkaMasala\GPIO\GPIOManager;
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Events\Dispatcher;
+
+/**
+ * Class EventSubscriber
+ * @package ChickenTikkaMasala\GPIO\Bridge\Laravel\Events
+ */
+class EventSubscriber
+{
+    /**
+     * @param Dispatcher $events
+     */
+    public function subscribe($events)
+    {
+        $events->listen(
+            'Illuminate\Foundation\Http\Events\RequestHandled',
+            get_class($this).'@updateCache'
+        );
+    }
+
+    /**
+     * @param GPIOManager $GPIOManager
+     * @return mixed
+     */
+    public function updateCache(RequestHandled $requestHandled)
+    {
+        Cache::forever(GPIOServiceProvider::CACHE_NAME, resolve(GPIOManager::class));
+    }
+}
