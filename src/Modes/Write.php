d670bb4e6261da185580c75d4b066a8cd4076832
ashleigh simonelli <ashleighsimonelli@gmail.com>
2017-05-18 11:32:20 +0100

    Removed obsolete function

--- src/Modes/Write.php
+++ src/Modes/Write.php
@@ -10,15 +10,7 @@ use ChickenTikkaMasala\GPIO\GPIO;
  */
 class Write extends GPIO
 {
-    /**
-     * @param int $value
-     * @return mixed
-     */
-    public function set($value = 0)
-    {
-        $this->lastValue = (int)($value >1) ? 1: (($value <1) ? 0 : $value);
-        return $this->execute();
-    }
+    public static $max = 1;
 
     /**
      * @return int
